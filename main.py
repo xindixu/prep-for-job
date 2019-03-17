52b9061feb7f3645f60949df71091f280a11dfa0
periodicaidan <periodicaidan@gmail.com>
2019-03-16 21:45:00 -0500

    Starting over without React

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template
 
-app = Flask(__name__, static_folder='../frontend/build/static', template_folder='../frontend/build')
+app = Flask(__name__)
 
 
 @app.route('/')
