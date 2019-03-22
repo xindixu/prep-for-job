34ce54737bbb7ff83421acdd8912c6a9d62dba88
Xindi Xu <xindixu@utexas.edu>
2019-03-21 22:38:49 -0500

    add requirements file

---
+++ frontend/build/precache-manifest.3193825e0f95ba49130afa464d2188c8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9334dbbbf88dfd4006ac",
+    "url": "/static/css/main.9f9f139e.chunk.css"
+  },
+  {
+    "revision": "9334dbbbf88dfd4006ac",
+    "url": "/static/js/main.b8acf70c.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "a539ea9d3b1acab9c730",
+    "url": "/static/js/2.948d4d77.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "cea0cfb6602e94af4a533a13286365fe",
+    "url": "/index.html"
+  }
+];
