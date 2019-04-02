d1f2f170f025328d790732cbe7c024ffe00fa65a
Xindi Xu <xindixu@utexas.edu>
2019-04-02 13:54:22 -0500

    ready to refactor

--- app/main.py
+++ app/main.py
@@ -15,80 +15,80 @@ import datetime
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
-    # app.config.from_mapping(
-    #     SECRET_KEY='dev',
-    #     DATABASE=os.path.join(app.instance_path, 'app.sqlite'),
-    #     SQLALCHEMY_DATABASE_URI='',
-    #     SQLALCHEMY_TRACK_MODIFICATIONS=False
-    # )
-    #
-    # db = SQLAlchemy(app)
-    #
-    # class Jobs (db.Model):
-    #     __tablename__ = "jobs"
-    #     id = db.Column(db.Integer, primary_key=True)
-    #     created_at = db.Column(db.DateTime,nullable = False)
-    #     updated_at = db.Column(db.DateTime,nullable = False)
-    #     title = db.Column(db.String(255), nullable=False)
-    #     salary = db.Column(db.Numeric)
-    #     description = db.Column(db.Text, nullable = False)
-    #     parent_skill = db.Column(db.String(255), nullable = False)
-    #
-    # class Skills (db.Model):
-    #     __tablename__ = "skills"
-    #     id = db.Column(db.Integer, primary_key=True)
-    #     created_at = db.Column(db.DateTime,nullable = False)
-    #     updated_at = db.Column(db.DateTime,nullable = False)
-    #     title = db.Column(db.String(255), nullable=False)
-    #     # description is nullable
-    #     description = db.Column(db.Text, nullable = True)
-    #     # check if parent skill can be null
-    #     parent_skill = db.Column(db.String(255), nullable = True)
-    #     importance = db.Column(db.Real, nullable = False)
-    #
-    # class Users (db.Model):
-    #     __tablename__ = "users"
-    #     hash = db.Column(db.String(256), nullable=False)
-    #     username = db.Column(db.String(256), nullable = False)
-    #     is_admin = db.Column(db.Boolean, nullable = False)
-    #     bio = db.Column(db.Text)
-    #     id = db.Column(db.Integer, primary_key=True)
-    #     image = db.Column(db.String(500))
-    #
-    #     def __repr__(self):
-    #         return f"User({self.id}, {self.email})"
-    #
-    #     @classmethod
-    #     def new_member(cls, email, password):
-    #         u = cls(email=email, hash=password)
-    #         db.session.add(u)
-    #         db.session.commit()
-    #         return u
-    #         # exists = db.engine.execute(text("SELECT * FROM users WHERE email='{}';".format(email))).execution_options(autocommit=True)
-    #         # if exists:
-    #         #     return False
-    #         # else:
-    #         #     # hash the password first
-    #         #     hashed_pass = "jhwfiwf" #todo
-    #         #     if db.engine.execute("INSERT INTO users (id, email, hash) VALUES(null, '{}', '{}')';".format(email, hashed_pass)).execution_options(autocommit=True):
-    #         #         return True
-    #         #     else:
-    #         #         return False
-    #
-    #     @classmethod
-    #     def view_members(cls):
-    #         return cls.query.all()
-    #
-    #     @classmethod
-    #     def check_password(cls, email, hpassword):
-    #         #todo hash password before passing
-    #         u = cls.query.filter_by(email=email).first()
-    #         if hpassword == u.hash:
-    #             return True
-    #         else:
-    #             return False
-    #
-    # db.create_all()
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+        DATABASE=os.path.join(app.instance_path, 'app.sqlite'),
+        SQLALCHEMY_DATABASE_URI='',
+        SQLALCHEMY_TRACK_MODIFICATIONS=False
+    )
+
+    db = SQLAlchemy(app)
+
+    class Jobs (db.Model):
+        __tablename__ = "jobs"
+        id = db.Column(db.Integer, primary_key=True)
+        created_at = db.Column(db.DateTime,nullable = False)
+        updated_at = db.Column(db.DateTime,nullable = False)
+        title = db.Column(db.String(255), nullable=False)
+        salary = db.Column(db.Numeric)
+        description = db.Column(db.Text, nullable = False)
+        parent_skill = db.Column(db.String(255), nullable = False)
+
+    class Skills (db.Model):
+        __tablename__ = "skills"
+        id = db.Column(db.Integer, primary_key=True)
+        created_at = db.Column(db.DateTime,nullable = False)
+        updated_at = db.Column(db.DateTime,nullable = False)
+        title = db.Column(db.String(255), nullable=False)
+        # description is nullable
+        description = db.Column(db.Text, nullable = True)
+        # check if parent skill can be null
+        parent_skill = db.Column(db.String(255), nullable = True)
+        importance = db.Column(db.Real, nullable = False)
+
+    class Users (db.Model):
+        __tablename__ = "users"
+        hash = db.Column(db.String(256), nullable=False)
+        username = db.Column(db.String(256), nullable = False)
+        is_admin = db.Column(db.Boolean, nullable = False)
+        bio = db.Column(db.Text)
+        id = db.Column(db.Integer, primary_key=True)
+        image = db.Column(db.String(500))
+
+        def __repr__(self):
+            return f"User({self.id}, {self.email})"
+
+        @classmethod
+        def new_member(cls, email, password):
+            u = cls(email=email, hash=password)
+            db.session.add(u)
+            db.session.commit()
+            return u
+            # exists = db.engine.execute(text("SELECT * FROM users WHERE email='{}';".format(email))).execution_options(autocommit=True)
+            # if exists:
+            #     return False
+            # else:
+            #     # hash the password first
+            #     hashed_pass = "jhwfiwf" #todo
+            #     if db.engine.execute("INSERT INTO users (id, email, hash) VALUES(null, '{}', '{}')';".format(email, hashed_pass)).execution_options(autocommit=True):
+            #         return True
+            #     else:
+            #         return False
+
+        @classmethod
+        def view_members(cls):
+            return cls.query.all()
+
+        @classmethod
+        def check_password(cls, email, hpassword):
+            #todo hash password before passing
+            u = cls.query.filter_by(email=email).first()
+            if hpassword == u.hash:
+                return True
+            else:
+                return False
+
+    db.create_all()
 
     if test_config is None:
         # load the instance config, if it exists, when not testing
