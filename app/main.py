143e641547f62c3cc0e5ff50d2fc6387941ed9f5
Srishtti Talwar <tsrishtti@gmail.com>
2019-04-04 16:12:31 -0500

    added few changes to styling of the salary page

--- app/main.py
+++ app/main.py
@@ -18,7 +18,7 @@ def create_app(test_config=None):
     app.config.from_mapping(
         SECRET_KEY='dev',
         DATABASE=os.path.join(app.instance_path, 'app.sqlite'),
-        SQLALCHEMY_DATABASE_URI='postgresql://postgres:postgres@localhost:5434/prep-for-job',
+        SQLALCHEMY_DATABASE_URI='postgresql://postgres:dbPassword1@157.230.173.38:5432/maindb4',
         SQLALCHEMY_TRACK_MODIFICATIONS=False
     )
 
